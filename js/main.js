1b3ee56f64fdd5c4473c92fe11746c0abeab0c6e
Jordan Aguirre <jjordannaguirre@gmail.com>
2024-06-08 16:52:39 -0300

    UPDATE

---
+++ js/main.js
@@ -0,0 +1,82 @@
+// Crear tres (3) variables para asignar el nombre, apellido y edad de Homero, respectivamente. Es indistinto el empleo de var o let en la declaración.
+// let nombre = 'Homero';
+// let apellido = 'Simpson';
+// let edad = 37;
+
+// Crear cinco (5) variables constantes, asignando a cada una de ellas diferentes nombres de ciudades. Generalmente, dichas variables se declaran con const.
+
+// const ciudad1 = 'Barcelona';
+// const ciudad2 = 'Madrid';
+// const ciudad3 = 'Cordoba';
+// const ciudad4 = 'Buenos Aires'
+// const ciudad5 = 'New York';
+
+// Declarar variables para representar la información de un carnet de conducir.
+// Luego, concatenarlas y asignar el resultado de la operación a una variable denominada carnet.
+
+// let nombre = '';
+// let fechaDeNacimiento = '';
+// let direccion = '';
+// let dni = '';
+
+// let carnet = '- Nombre: ' + nombre +
+//             '- Fecha de nacimiento: ' + fechaDeNacimiento +
+//             '- Dirección: ' + direccion +
+//             '- DNI: ' + dni; 
+
+// Solicitar al usuario cinco (5) nombres pertenecientes a integrantes de una familia. 
+// Luego, concatenar dichas entradas y efectuar una salida por alerta.
+
+// let nombre1 = prompt('Ingresar nombre del primer integrante');
+// let nombre2 = prompt('Ingresar nombre del segundo integrante');
+// let nombre3 = prompt('Ingresar nombre del tercer integrante');
+// let nombre4 = prompt('Ingresar nombre del cuarto integrante');
+// let nombre5 = prompt('Ingresar nombre del quinto integrante');
+
+// let todosLosNombres = ' Hijo: ' + nombre1 +
+//                         ' Hija: ' + nombre2 +
+//                         ' Padre: ' + nombre3 +
+//                         ' Madre: ' + nombre4 +
+//                         ' Abuela: ' + nombre5;
+//             alert(todosLosNombres);
+
+// Solicitar al usuario uno o más precios.
+// Luego, efectuar un descuento en porcentajes (20%, 30%, etcétera) sobre los valores ingresados, y realizar una salida.
+
+// let precio = Number(prompt('Ingresar precio: '))
+
+// let descuento20 = precio * 0.2;
+// let descuento30 = precio * 0.3;
+
+// console.log(descuento20);
+// console.log(descuento30);
+
+// Solicitar al usuario un (1) nombre.
+// Si el mismo es igual a otro nombre almacenado en una variable, realizar una salida por alerta con el mensaje “Fui yo”. 
+// Caso contrario, la salida será “Yo no fui”.
+
+// let entrada = prompt('Ingrese un nombre: ');
+// let nombre2 = 'bart';
+// if (entrada == nombre2){
+//     alert('Fui yo')
+// } else{
+//     alert('Yo no fui')
+// }
+
+// Solicitar al usuario una (1) tecla.
+// Si se presiona “y” (minúscula), o “Y” (mayúscula), realizar una salida por alerta con el mensaje “Correcto”. 
+// Caso contrario, la salida será “Error”.
+
+// let tecla = prompt('Ingrese una tecla: ');
+// if ((tecla == 'y') || (tecla == 'Y')) { 
+//     alert('CORRECTO');
+// } else {
+//     alert ('ERROR');
+// }
+
+// let entrada2 = prompt("INGRESAR UN  TECLA");
+// if ((entrada2 == "y") || (entrada2 == "Y")) {
+//     alert("CORRECTO");
+// } else {
+//     alert("ERROR");
+// }
